7eb34dd0b94bcd024f1b02fb0b3cee403752f99d
Christopher Sexton <cpsexton@gmail.com>
2019-07-24 15:50:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var katzDeliLine = [];
+//var numberInLine = [];
+
+function takeANumber() {
+  katzDeliLine.push("Ada");
+  //numberInLine += 1;
+  return console.log("Welcome Ada. You are number", katzDeliLine.indexOf("Ada") + 1, "in line.");
+}
+
+
+/*
+function nowServing() {
+  let serving = katzDeliLine.slice(0);
+  return console.log("Currently serving ", serving, ".")
+   katzDeliLine.shift()
+}
+*/
